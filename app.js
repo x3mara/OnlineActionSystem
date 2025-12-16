749b644d3b761553d42aed9063d29c07c9cca8ae
Omar Akram <99609294+lolman9002@users.noreply.github.com>
2025-12-16 13:52:15 +0200

    app changed

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
+const session = require('express-session');
+const usercontroller = require('./Controller/userController');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './View');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+  secret:'secretKey',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
   res.send('<p>Hello World</p>');
 });
 
-// Serve static files from the "View" directory
-
 app.get('/error', (req, res) => {
   res.sendFile("./View/404.html" , { root: __dirname });
 });
+
+app.get('/dashboard', (req, res) => {
+  if (!req.session.user) {
+    return res.redirect('/register');
+  }
+  res.render('dashboard', { user: req.session.user });
+});
+
+app.post('/register', usercontroller.register);
 
 //error page
 app.use((req, res) => {
