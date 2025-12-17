69816f2841171c2b5809b292cee28435a8c3df20
Yahia_g <yahiaahmed2yahia@hotmail.com>
2025-12-17 19:59:50 +0200

    banana

--- Database/database.js
+++ Database/database.js
@@ -117,7 +117,7 @@ export async function getImgs(item_id){
   const [rows] = await pool.query(sql, [item_id]);
   return rows;
 }
-export async function saerchAuction(auctionID){
+export async function searchAuction(auctionID){
   const sql = 'select * from auction where id =?';
   const [rows] = await pool.query(sql, [auctionID]);
   return rows;
