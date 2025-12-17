db2f271cbac97020feb700204314c7bc8f968be5
Jasmin Ibrahim <jasminibrahimfathy123@gmail.com>
2025-12-17 12:12:03 +0200

    fixing returning messages

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -5,19 +5,18 @@ export default class clientController{
     
 
     async register(req, res){
-
         const { username,email, password} = req.body;
         if (password.length<8){
             //handle responses
-            return "Password must be at least 8 characters long";
+            res.render('SignUp' , {success: false, field: "password", message: "Password must be at least 8 characters long"});
         }
         if (password.length>50){
             //handle responses
-            return "Password must be less than 50 characters long";
+            res.render('SignUp' , {success: false, field: "password", message: "Password must be less than 50 characters long"});
         }
         if (email.length<5 || !email.includes('@')){
             //handle responses
-            return "Invalid email address";
+            res.render('SignUp' , {success: false, field: "email", message: "Invalid email address"});
         }
         const sessionWallet = new Wallet();
         let sessionClient = await Client.insertClient(username, password, email, sessionWallet.getWalletID());
@@ -25,14 +24,13 @@ export default class clientController{
         if (sessionClient == null){
             //handle responses
             sessionWallet=null;
-            return "Username already exists";
+            res.render('SignUp' , {success: false, field: "username", message: "Username already exists"});
         }
 
         Wallet.insertWallet(sessionWallet);
         req.session.wallet = sessionWallet;
         req.session.user = sessionClient;
-        res.render('ClientDashboard' , {success: false, field: "password", message: "Incorrect password"});
-
+        res.render('ClientDashboard' , {success: true, field: "", message: "Successfully registered"});
     }
 
     async login(req, res){
