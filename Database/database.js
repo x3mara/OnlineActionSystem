3ca8a4e424ed6311bc597b67acf6c5baf4e0abce
omar887 <cleveromar00@gmail.com>
2025-12-17 22:26:46 +0200

    done

--- Database/database.js
+++ Database/database.js
@@ -124,7 +124,7 @@ export async function searchAuction(auctionID){
   return rows;
 }
 export async function searchItem(item_id){
-  const sql = `SELECT item.*, itemimg.itemimg FROM item LEFT JOIN itemimg ON item.id = itemimg.item_id WHERE item.id = ? `;
+  const sql = `SELECT item.*' FROM item WHERE item.id =?`;
   const [rows] = await pool.query(sql, [item_id]);
   return rows;
 }
