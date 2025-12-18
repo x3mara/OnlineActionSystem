f9b8c09e8c85bef6cdacf34b976b618d2cf9d144
Ahmed Emara <ahmed6emara@gmail.com>
2025-12-18 02:28:41 +0200

    please

--- backend/controllers/auctionController.js
+++ backend/controllers/auctionController.js
@@ -4,17 +4,17 @@ import Auction from "../Auction.js";
 export default class auctionController{
 
     async startAuction(req, res){
-        console.log("hi");
         const {name, desc, buyout_price, starting_price, end_date, tag} 
             = req.body;
         const item = new Item(name, desc, tag, buyout_price, starting_price);
         await Item.insertItem(item);
-        console.log(req.session.user);
+        console.log("Session User: " + req.session.user);
         let seller = await User.searchClient(req.session.user.getUsername());
         const auction = new Auction(item,end_date,seller,starting_price);
         await Auction.insertAuction(auction);
-        console.log(item);
-        console.log(auction);
+        console.log("Item Details: " + item);
+        console.log("Auction Details: " + auction);
+        
     }
 
     
