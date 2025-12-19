e65a1990326acc84ad19b9f7d69364b29161ceaf
Ahmed Emara <ahmed6emara@gmail.com>
2025-12-19 19:03:19 +0200

    frontend card

--- app.js
+++ app.js
@@ -203,10 +203,10 @@ app.post('/card/add', async (req,res) => {
 
 app.get('/clientdashboard', async (req, res) => {
   console.log(req.session.user);
-  console.log(await ControllerCL.getCardDetails(req.session.user));
+  console.log((await ControllerCL.getCardDetails(req.session.user))[0]);
   res.render('ClientDashboard', {
     username: req.session.user,
-    card: await ControllerCL.getCardDetails(req.session.user),
+    card: (await ControllerCL.getCardDetails(req.session.user))[0],
     avatar: await ControllerCL.getAvatar(req.session.user),
     balance: await ControllerWA.getBalance(req.session.user),
     recommendedAuctions: await ControllerAU.getRecommendedAuctions(req.session.user),
