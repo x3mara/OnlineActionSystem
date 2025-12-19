f563afdd7480f4e75784278444aaf04307d4a808
Omar Akram <99609294+lolman9002@users.noreply.github.com>
2025-12-19 04:08:29 +0200

    her name was lola

--- backend/Auction.js
+++ backend/Auction.js
@@ -2,7 +2,7 @@ import Bid from "./Bid.js";
 import Client from "./Client.js";
 import clientController from "./controllers/clientController.js";
 import walletController from "./controllers/walletController.js";
-import {searchAuction, updateduedate, searchWalletbyWalletID, searchWalletbyid, searchItem, insert, viewBidsforAuction, searchClientbyid, updatewalletbalance, updatehighestbid} from "../Database/database.js";
+import {searchAuction, updateduedate, searchWalletbyWalletID, searchWalletbyid, searchItem, insert, viewBidsforAuction, searchClientbyid, updatewalletbalance, updatehighestbid , getEverythingWithItem , getImgs} from "../Database/database.js";
 
 export default class Auction {
     auctionID;//int
@@ -140,5 +140,16 @@ export default class Auction {
         return this.highestBidder;
     }   
 
-    
+    async getAuctiondetails(AuctionID){
+        return getEverythingWithItem(AuctionID);
+    }
+
+    async getSellerName(AuctionID){
+        return getEverythingWithItem(getImgs);
+    }
+
+    async getAllbidders(AuctionID){
+        return getAllbidsforAuction(AuctionID);
+    }
+
 }
